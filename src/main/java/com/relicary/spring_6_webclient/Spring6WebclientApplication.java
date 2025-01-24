8b1ff6a3f37abe5d41e2b3e6d28642f298a5fb75
Juan Cremades <relicary@gmail.com>
2025-01-24 13:41:46 +0100

    Initial code

---
+++ src/main/java/com/relicary/spring_6_webclient/Spring6WebclientApplication.java
@@ -0,0 +1,13 @@
+package com.relicary.spring_6_webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6WebclientApplication.class, args);
+	}
+
+}
